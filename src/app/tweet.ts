5a5645d8c33bc960b8590a2dce4e8e9cfbb122dd
Glen Smith <glen@bytecode.com.au>
2016-08-30 16:53:25 +1000

    Fixed bug in tweet avatar casing

--- src/app/tweet.ts
+++ src/app/tweet.ts
@@ -4,7 +4,7 @@ export class Tweet {
     public avatar;
 
     constructor(public body: string, public author: string, public date: Date, public retweets: Array<string>, public favorites: Array<string>) {
-        this.avatar = `${author}.jpg`;
+        this.avatar = `${author.toLowerCase()}.jpg`;
     }
 
     hasFavorited(userId : string) : boolean {
